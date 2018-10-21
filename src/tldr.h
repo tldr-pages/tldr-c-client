15dd037e9b5f662b43900c54d69876f67e36f78c
Aidan Coyle <packrat386@gmail.com>
2018-10-21 14:46:21 -0500

    Add --list option
    This will list each of the commands for which there is an entry in the
    current local database. It lists only packages for the current
    platform or can be overriden by the --platform flag. It will update
    the local database if there is not one.

--- src/tldr.h
+++ src/tldr.h
@@ -68,6 +68,8 @@ int         construct_path          (char *buf, size_t buflen, char const *home,
                                      char const *input, char const *platform);
 int         parse_tldrpage          (char const *input);
 int         print_tldrpage          (char const *input, char const *platform);
+int         print_tldrlist          (char const *platform);
+int         parse_tldrlist          (char const *path, char const *platform);
 int         print_localpage         (char const *path);
 
 /* utils.c */
