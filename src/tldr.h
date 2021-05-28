1aac52e209fdd2175c1cce5accfd2a8e3d1e9d79
marchersimon <marchersimon@zohomail.eu>
2021-05-28 23:28:05 +0200

    replace master with main

--- src/tldr.h
+++ src/tldr.h
@@ -14,19 +14,19 @@
 #define STRBUFSIZ 512
 #define URLBUFSIZ 1024
 
-#define BASE_URL "https://raw.github.com/tldr-pages/tldr/master/pages"
+#define BASE_URL "https://raw.github.com/tldr-pages/tldr/main/pages"
 #define BASE_URL_LEN (sizeof(BASE_URL) - 1)
 
-#define ZIP_URL "https://github.com/tldr-pages/tldr/archive/master.zip"
+#define ZIP_URL "https://github.com/tldr-pages/tldr/archive/main.zip"
 #define ZIP_URL_LEN (sizeof(ZIP_URL_LEN) - 1)
 
 #define TMP_DIR "/tmp/tldrXXXXXX"
 #define TMP_DIR_LEN (sizeof(TMP_DIR) - 1)
 
-#define TMP_FILE "/master.zip"
+#define TMP_FILE "/main.zip"
 #define TMP_FILE_LEN (sizeof(TMP_FILE) - 1)
 
-#define TLDR_DIR "/tldr-master"
+#define TLDR_DIR "/tldr-main"
 #define TLDR_DIR_LEN (sizeof(TLDR_DIR) - 1)
 
 #define TLDR_HOME "/.tldrc"
@@ -35,7 +35,7 @@
 #define TLDR_DATE "/.tldrc/date"
 #define TLDR_DATE_LEN (sizeof(TLDR_DATE) - 1)
 
-#define TLDR_EXT "/.tldrc/tldr-master/pages/"
+#define TLDR_EXT "/.tldrc/tldr-main/pages/"
 #define TLDR_EXT_LEN (sizeof(TLDR_EXT) - 1)
 
 #define ANSI_COLOR_RESET_FG                     "\x1b[39m"
