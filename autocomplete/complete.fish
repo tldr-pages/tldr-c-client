2dc6b419df75d738f37c60f9895534dc9243ce2c
Ethan Kinnear <51250849+superatomic@users.noreply.github.com>
2022-05-04 22:06:52 -0500

    Remove redundant options in fish completion
    The documentation for `complete` lists `-x` as an alias for `-rf`.
    
    This allows for some options to be removed because their functionality
    is already enabled by other flags.
    
    > -x or --exclusive
    >
    >     Short for -r and -f.
    
    Source: <https://fishshell.com/docs/current/cmds/complete.html>
    
    This change is just refactoring. No functionality is different compared
    to the last commit.

--- autocomplete/complete.fish
+++ autocomplete/complete.fish
@@ -1,9 +1,9 @@
 complete -c tldr -f -s v -d "print verbose output"
-complete -c tldr -xf -l version -d "print version and exit" -n "__fish_use_subcommand"
-complete -c tldr -xf -s h -l help -d "print this help and exit" -n "__fish_use_subcommand"
-complete -c tldr -xf -s u -l update -d "update local database" -n "__fish_use_subcommand"
-complete -c tldr -xf -s c -l clear-cache -d "clear local database" -n "__fish_use_subcommand"
-complete -c tldr -rf -f -s p -l platform -a "linux osx sunos common" -d "select platform, supported are linux / osx / sunos / common"
+complete -c tldr -x -l version -d "print version and exit" -n "__fish_use_subcommand"
+complete -c tldr -x -s h -l help -d "print this help and exit" -n "__fish_use_subcommand"
+complete -c tldr -x -s u -l update -d "update local database" -n "__fish_use_subcommand"
+complete -c tldr -x -s c -l clear-cache -d "clear local database" -n "__fish_use_subcommand"
+complete -c tldr -x -s p -l platform -a "linux osx sunos common" -d "select platform, supported are linux / osx / sunos / common"
 complete -c tldr -r -s r -l render -F -d "render a local page for testing purpose" -n "__fish_use_subcommand"
 
 function __tldr_get_files
