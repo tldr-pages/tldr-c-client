a839565e4b43fcb8e4768fd57c6b52830f11e736
Ethan Kinnear <51250849+superatomic@users.noreply.github.com>
2022-05-04 21:58:52 -0500

    Make fish-shell autocompletion smarter
    `tldr` fish autocompletion is now functional, but the autocompletion
    will sometimes suggest things that are invalid, such as:
    
     - Suggesting more tldr-page names after autocompleting the first, even
       though you can only get one tldr-page at a time.
     - Recommending incompatible option combinations, like suggesting
       `--update`, `--help`, and `--render` at the same time.
     - Suggesting multiple file paths after specifying `--render`, even
       though that isn't valid syntax.
    
    This commit resolves these problems by checking to see what values have
    already been entered. This makes the suggestions more "intelligent".
    
    Documentation: <https://fishshell.com/docs/current/cmds/complete.html>,
    <https://fishshell.com/docs/current/completions.html#completion-own>.

--- autocomplete/complete.fish
+++ autocomplete/complete.fish
@@ -1,10 +1,10 @@
-complete -c tldr -xf -s v -d "print verbose output"
-complete -c tldr -xf -l version -d "print version and exit"
-complete -c tldr -xf -s h -l help -d "print this help and exit"
-complete -c tldr -xf -s u -l update -d "update local database"
-complete -c tldr -xf -s c -l clear-cache -d "clear local database"
+complete -c tldr -f -s v -d "print verbose output"
+complete -c tldr -xf -l version -d "print version and exit" -n "__fish_use_subcommand"
+complete -c tldr -xf -s h -l help -d "print this help and exit" -n "__fish_use_subcommand"
+complete -c tldr -xf -s u -l update -d "update local database" -n "__fish_use_subcommand"
+complete -c tldr -xf -s c -l clear-cache -d "clear local database" -n "__fish_use_subcommand"
 complete -c tldr -rf -f -s p -l platform -a "linux osx sunos common" -d "select platform, supported are linux / osx / sunos / common"
-complete -c tldr -r -s r -l render -F -d "render a local page for testing purpose"
+complete -c tldr -r -s r -l render -F -d "render a local page for testing purpose" -n "__fish_use_subcommand"
 
 function __tldr_get_files
     set -l files (basename -s .md (find $HOME/.tldrc/tldr/pages/$argv[1] -name '*.md'))
@@ -25,7 +25,8 @@ if test -d "$HOME/.tldrc/tldr/pages"
         set cmpl $cmpl (__tldr_get_files sunos)
     end
 
-    complete -c tldr -f -a (echo $cmpl | sort | uniq)
+    complete -c tldr -f -a (echo $cmpl | sort | uniq) -n "__fish_use_subcommand"
+    complete -c tldr -f
 end
 
 functions -e __tldr_get_files
