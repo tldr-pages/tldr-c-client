45fe27ff650387a5ed23965a98bfb02dc7ea30d8
Ethan Kinnear <51250849+superatomic@users.noreply.github.com>
2022-06-23 14:05:33 -0500

    Format options' `complete` lines to align them
    This change is just refactoring. No functionality is different compared
    to the last commit.

--- autocomplete/complete.fish
+++ autocomplete/complete.fish
@@ -1,10 +1,10 @@
-complete -c tldr -f -s v -d "print verbose output"
-complete -c tldr -x -l version -d "print version and exit" -n __fish_use_subcommand
-complete -c tldr -x -s h -l help -d "print this help and exit" -n __fish_use_subcommand
-complete -c tldr -x -s u -l update -d "update local database" -n __fish_use_subcommand
-complete -c tldr -x -s c -l clear-cache -d "clear local database" -n __fish_use_subcommand
-complete -c tldr -x -s p -l platform -a "linux osx sunos common" -d "select platform, supported are linux / osx / sunos / common"
-complete -c tldr -r -s r -l render -F -d "render a local page for testing purpose" -n __fish_use_subcommand
+complete -c tldr -f                              -s v                -d "print verbose output"
+complete -c tldr -x  -n __fish_use_subcommand         -l version     -d "print version and exit"
+complete -c tldr -x  -n __fish_use_subcommand    -s h -l help        -d "print this help and exit"
+complete -c tldr -x  -n __fish_use_subcommand    -s u -l update      -d "update local database"
+complete -c tldr -x  -n __fish_use_subcommand    -s c -l clear-cache -d "clear local database"
+complete -c tldr -x  -a "linux osx sunos common" -s p -l platform    -d "select platform, supported are linux / osx / sunos / common"
+complete -c tldr -rF -n __fish_use_subcommand    -s r -l render      -d "render a local page for testing purpose"
 
 function __tldr_get_files
     basename -s .md (find $HOME/.tldrc/tldr/pages/$argv[1] -name '*.md') | string escape
