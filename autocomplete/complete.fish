286bba2e328db339e85f2d6fff0f818568b31571
Ethan Kinnear <51250849+superatomic@users.noreply.github.com>
2022-06-23 13:10:48 -0500

    Remove quotes around `__fish_use_subcommand`
    Suggested by https://github.com/johnflavin-fw
    
    This change is just refactoring. No functionality is different compared
    to the last commit.

--- autocomplete/complete.fish
+++ autocomplete/complete.fish
@@ -1,10 +1,10 @@
 complete -c tldr -f -s v -d "print verbose output"
-complete -c tldr -x -l version -d "print version and exit" -n "__fish_use_subcommand"
-complete -c tldr -x -s h -l help -d "print this help and exit" -n "__fish_use_subcommand"
-complete -c tldr -x -s u -l update -d "update local database" -n "__fish_use_subcommand"
-complete -c tldr -x -s c -l clear-cache -d "clear local database" -n "__fish_use_subcommand"
+complete -c tldr -x -l version -d "print version and exit" -n __fish_use_subcommand
+complete -c tldr -x -s h -l help -d "print this help and exit" -n __fish_use_subcommand
+complete -c tldr -x -s u -l update -d "update local database" -n __fish_use_subcommand
+complete -c tldr -x -s c -l clear-cache -d "clear local database" -n __fish_use_subcommand
 complete -c tldr -x -s p -l platform -a "linux osx sunos common" -d "select platform, supported are linux / osx / sunos / common"
-complete -c tldr -r -s r -l render -F -d "render a local page for testing purpose" -n "__fish_use_subcommand"
+complete -c tldr -r -s r -l render -F -d "render a local page for testing purpose" -n __fish_use_subcommand
 
 function __tldr_get_files
     set -l files (basename -s .md (find $HOME/.tldrc/tldr/pages/$argv[1] -name '*.md'))
@@ -25,7 +25,7 @@ if test -d "$HOME/.tldrc/tldr/pages"
         set cmpl $cmpl (__tldr_get_files sunos)
     end
 
-    complete -c tldr -f -a (echo $cmpl | sort | uniq) -n "__fish_use_subcommand"
+    complete -c tldr -f -a (echo $cmpl | sort | uniq) -n __fish_use_subcommand
     complete -c tldr -f
 end
 
