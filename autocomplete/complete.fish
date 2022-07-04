87cfcd6993ac195c43687d3764dcf8d4d2f5cb71
Ethan Kinnear <contact@superatomic.dev>
2022-07-03 23:44:10 -0400

    Various fixes and improvements to fish shell completion (#86)
    Co-authored-by: John Flavin <flavinj@gmail.com>

--- autocomplete/complete.fish
+++ autocomplete/complete.fish
@@ -1,16 +1,48 @@
-complete -c tldr -xf -s v -d "print verbose output"
-complete -c tldr -xf -l version -d "print version and exit"
-complete -c tldr -xf -s h -l help -d "print this help and exit"
-complete -c tldr -xf -s u -l update -d "update local database"
-complete -c tldr -xf -s c -l clear-cache -d "clear local database"
-complete -c tldr -rf -f -s p -l platform -a "linux osx sunos common" -d "select platform, supported are linux / osx / sunos / common"
-complete -c tldr -r -s r -l render -a PATH -d "render a local page for testing purpose"
+function __tldr_not_contain_standalone_opt
+        __fish_not_contain_opt -s v
+    and __fish_not_contain_opt      version
+    and __fish_not_contain_opt -s l list
+    and __fish_not_contain_opt -s h help
+    and __fish_not_contain_opt -s u update
+    and __fish_not_contain_opt -s c clear-cache
+end
+
+function __tldr_no_os_choice_opt
+        __fish_not_contain_opt      linux osx sunos
+    and __fish_not_contain_opt -s r render
+    and __tldr_not_contain_standalone_opt
+end
+
+function __tldr_no_os_choice_opt_nor_p
+        __tldr_no_os_choice_opt
+    and __fish_not_contain_opt -s p platform
+end
+
+function __tldr_positional
+        __fish_use_subcommand
+    and __tldr_not_contain_standalone_opt
+end
+
+function __tldr_positional_no_os
+        __tldr_positional
+    and __fish_not_contain_opt      linux osx sunos
+    and __fish_not_contain_opt -s p platform
+end
+
+complete -c tldr -f  -n "__fish_not_contain_opt -s v"  -s v                -d "print verbose output"
+complete -c tldr -f  -n  __fish_is_first_arg                -l version     -d "print version and exit"
+complete -c tldr -f  -n  __fish_is_first_arg           -s l -l list        -d "list all entries in the local database"
+complete -c tldr -f  -n  __fish_is_first_arg           -s h -l help        -d "print help and exit"
+complete -c tldr -f  -n  __fish_is_first_arg           -s u -l update      -d "update local database"
+complete -c tldr -f  -n  __fish_is_first_arg           -s c -l clear-cache -d "clear local database"
+complete -c tldr -x  -n  __tldr_no_os_choice_opt       -s p -l platform    -d "select platform" -a "linux osx sunos common"
+complete -c tldr -f  -n  __tldr_no_os_choice_opt_nor_p      -l linux       -d "show command page for Linux"
+complete -c tldr -f  -n  __tldr_no_os_choice_opt_nor_p      -l osx         -d "show command page for macOS"
+complete -c tldr -f  -n  __tldr_no_os_choice_opt_nor_p      -l sunos       -d "show command page for SunOS"
+complete -c tldr -rF -n  __tldr_positional_no_os       -s r -l render      -d "render a local page for testing purposes"
 
 function __tldr_get_files
-    set -l files (basename -s .md (find $HOME/.tldrc/tldr/pages/$argv[1] -name '*.md'))
-    for f in $files
-        echo $f
-    end
+    basename -s .md (find $HOME/.tldrc/tldr/pages/$argv[1] -name '*.md') | string escape
 end
 
 if test -d "$HOME/.tldrc/tldr/pages"
@@ -25,7 +57,9 @@ if test -d "$HOME/.tldrc/tldr/pages"
         set cmpl $cmpl (__tldr_get_files sunos)
     end
 
-    complete -c tldr -a (echo $cmpl | sort | uniq)
+    complete -c tldr -f -a "$cmpl" -n __tldr_positional
 end
+
+complete -c tldr -f
 
 functions -e __tldr_get_files
