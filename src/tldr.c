7f2d816e6d0315dfcf65221f90de1c7e08ac50a1
4G3NT <zoolduel@gmail.com>
2023-07-27 05:35:32 -0700

    Revert to old usage, but use the long option instead

--- src/tldr.c
+++ src/tldr.c
@@ -14,11 +14,6 @@
 #include <time.h>
 
 #define VERSION_TAG "v1.6.0"
-#ifndef VERSION
-    #define VERSION_PRETTY ""
-#else
-    #define VERSION_PRETTY VERSION
-#endif
 
 /* Help and usage */
 void        print_version           (char const *arg);
@@ -196,20 +191,13 @@ main(int argc, char **argv)
 void
 print_version(char const *arg)
 {
-    /* *INDENT-OFF* */
-    if (strcmp("", VERSION_PRETTY) == 0)
-       fprintf(stdout, "%s %s\n", arg, VERSION_TAG);
-    else
-       fprintf(stdout, "%s %s (%s)\n", arg, VERSION_TAG, VERSION_PRETTY);;
-    fprintf(stdout, "Copyright (C) 2016 Arvid Gerstmann\n");
-    fprintf(stdout, "Source available at https://github.com/tldr-pages/tldr-c-client\n");
-    /* *INDENT-ON* */
+    fprintf(stdout, "%s %s\n", arg, VERSION_TAG);
 }
 
 void
 print_usage(char const *arg)
 {
-    char const *out = "usage: %s [-v] [-c] [-u] [OPTION]... PAGE\n\n";
+    char const *out = "usage: %s [--verbose] [OPTION]... PAGE\n\n";
 
     /* *INDENT-OFF* */
     fprintf(stdout, out, arg);
